559c60fc6b8b45bf647e25a5b7494af7be7c715e
kasou2018 <q5653831@gmail.com>
2019-11-11 22:45:57 +0900

    passwordのHash化

--- src/test/java/com/RESTJWT/demo/DemoApplicationTests.java
+++ src/test/java/com/RESTJWT/demo/DemoApplicationTests.java
@@ -1,13 +1,19 @@
 package com.RESTJWT.demo;
 
+import io.jsonwebtoken.lang.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootTest
 class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+		String hash = bCryptPasswordEncoder.encode("password");
+		System.out.println(hash);
+		Assert.isTrue(bCryptPasswordEncoder.matches("password", hash));
 	}
 
 }
